4613b68b9033cd6c50ec3938787fcaa9f3264d3a
nerdondon <willdon@nerdondon.com>
2018-08-26 18:14:48 -0700

    Update workspace paths

--- zsh/omz/custom/env.zsh
+++ zsh/omz/custom/env.zsh
@@ -9,26 +9,25 @@ export NVM_DIR="$HOME/.nvm"
 
 # workspaces
 # set paths to commonly used directories
-# set paths to commonly used directories
 export XAMPP=/Applications/XAMPP/xamppfiles
 export PRIMARY="/Users/williamseandon/Data/PrimaryWorkspace"
 export ECLIPSE="/Users/williamseandon/Data/Eclipse Workspace"
 export FINANCE="/Users/williamseandon/Data/Finance Workspace"
 export PYTHON="/Users/williamseandon/Data/Python Workspace"
 export BLOG="${PRIMARY}/nerdondon.github.io"
-export HR="${PYTHON}/HackerRank Scripts"
+export SANDBOX="${PRIMARY}/sandbox"
 export GOPATH="${PRIMARY}/go"
 
 # set aliases to jump to workspaces
 alias cdp='cd "${PRIMARY}"'
-alias cdPrim='cd "${PRIMARY}"'
 alias cdEclipse='cd "${ECLIPSE}"'
 alias cdFinance='cd "${FINANCE}"'
 alias cdBlog='cd "${BLOG}"'
 alias cdXAMPP='cd "${XAMPP}"'
 alias cdGo='cd "${GOPATH}"'
-alias directoryAliases='echo -e "\033[1;35mCD ALIASES: cd[p | Go | Blog | Eclipse | Finance | XAMPP]\033[0m"'
+alias cds='cd "${SANDBOX}"'
+alias directoryAliases='echo -e "\033[1;35mCD ALIASES: cd[p | s | Go | Blog | Eclipse | Finance | XAMPP]\033[0m"'
 directoryAliases
 
 # js scratchpad
-alias scratch='code "${HR}/"'
+alias scratch='code "${SANDBOX}/"'
