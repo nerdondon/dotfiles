a05240e90f740e2a4d5fa69926ad4a1d90c0739e
nerdondon <willdon@nerdondon.com>
2018-08-30 01:36:20 -0700

    Change DOTFILES env variable to not collide with omz's ZSH variable

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,1 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/Applications/XAMPP/xamppfiles/clean_bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/Applications/XAMPP/xamppfiles/clean_bin:$PATH"
