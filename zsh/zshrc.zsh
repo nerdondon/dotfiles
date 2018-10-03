d6299cff1792005c487d6e346c5bbb32246e3515
nerdondon <willdon@nerdondon.com>
2018-10-03 13:27:27 -0700

    Fix location of custom folder

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -55,7 +55,7 @@ ZSH_THEME="spaceship"
 # HIST_STAMPS="mm/dd/yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
-ZSH_CUSTOM=$HOME/.dotfiles/zsh/omz/custom
+ZSH_CUSTOM=$HOME/dotfiles/zsh/omz/custom
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
